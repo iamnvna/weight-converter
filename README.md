d9685e2a29ba80ebf52fd18416cb3f5e20a2945d
iamnvna <tscobbinah@gmail.com>
2022-01-26 14:07:23 +0000

    Final commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+### Project Ten - 100DaysOfCode
+# Weight Converter
+This is a simple program that accepts a weight value from a user in either Pounds or Kilogram, and converts it to the other.
+
+## Background
+This project is part of my #100DaysOfCode learning streak. I intend to write a simple python program each day. Each project would:
+* be heavily commented for learning purposes;
+* include a README file similar to this one;
+* include an algorithm to solve it without code;
+* include a summary of what I learnt.
+
+## Algorithm
+1. Start.
+2. Create a variable to hold the user input and converted weight.
+3. Create a variable to hold the current weight measure from the user.
+4. Use if statements to evaluate the user input from both step 2 and 3. Where invalid inputs are entered, prompt the user. Error handling is another python technique that can be used here.
+5. Use if statements to perform the appropriate math operation to convert the weight value.
+8. Print the converted weight value.
+9. Stop.
+
+## What I've learnt
+* I explored the use of error handling in Python for the first time instead of a series of if statements to catch an error. I'm exploring it more.
+
+For more understanding on Error Handling, **[read](https://docs.python.org/3/tutorial/errors.html)** the full documentation.
--- main.py
+++ main.py
@@ -3,23 +3,48 @@ print("Welcome to weight converter!"
 
 weight = 0
 convert = 0
+
 first_weight = input("\nWhich weight option are you converting from? K/P: ").lower()
-
 while True:
-      if first_weight not in ['k', 'p']:
-            print("Enter a valid option.")
-      else:
+    if first_weight not in ['k', 'p']:
+        print("Enter a valid option.")
+    else:
+        try:
             weight = float(input("Enter the weight value: "))
-
-      if first_weight == 'k':
-            convert = (weight * 2.2046)
-            print(weight, "pounds converts to", convert, "kilograms.")
+        except ValueError:
+            print("Oops! That was no valid number. Try again.")
             break
-      else:
-            convert = (weight / 2.2046)
-            print(weight, "kilograms converts to", convert, "pounds.")
-            break
+    
+    if first_weight == 'k':
+        convert = (weight * 2.2046)
+        print(weight, "pounds converts to", convert, "kilograms.")
+        break
+    else:
+        convert = (weight / 2.2046)
+        print(weight, "kilograms converts to", convert, "pounds.")
+        break
 
+# Commentary
+"""
+Line 1:
+      The print statement welcomes the user to the program, and explains the program
+      function.
 
+Line 4-5:
+      The variables to hold the weight input from the user and the converted values
+      are declared here.
 
+Line 7:
+      Line 7 asks for input from the user, the user enters the weight measure they are
+      converting from.
 
+Line 8-25:
+      while True allows the code block below it rerun anytime it meets the continue
+      keyword. The block of code first checks to be sure the user entered either k or p.
+      If not, they are prompted to enter a valid input. Where the user entered either
+      k or p, the user is further asked to input the weight value to be converted. Error
+      handling is employed here to make sure that any other input aside an integer or float
+      from the user is thrown as an error and "Oops! That was no valid number. Try again."
+      is printed on the screen. The code from line 9 is evaluated against the if statement
+      in line 18, and the appropriate code runs.
+"""
